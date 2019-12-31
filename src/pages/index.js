dcdc921e36e489df8936858746be292c76dedb2b
Elsayed Awdallah <comando4ever@gmail.com>
2019-12-31 23:32:19 +0200

    remove useless elses

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,13 @@ const isLeap = (year) => {
 
     if (year % 4 !== 0) {
         return false;
-    } else if (year % 100 !== 0) {
+    }
+
+    if (year % 100 !== 0) {
         return true;
-    } else if (year % 400 !== 0) {
+    }
+
+    if (year % 400 !== 0) {
         return false;
     }
 
