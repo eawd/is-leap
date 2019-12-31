c221a486f2f8aab97cf90207d41ccab1f3b96438
Elsayed Awdallah <comando4ever@gmail.com>
2019-12-31 23:55:59 +0200

    move each link to separate line

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,9 @@ export default () => {
 
         <br />
 
-        <a href="https://eawd.me">ارجع للرئيسية</a> | <a href="https://ar.wikipedia.org/wiki/%D8%B3%D9%86%D8%A9_%D9%83%D8%A8%D9%8A%D8%B3%D8%A9" target="_blank">معلومات اكتر</a>
+        <a href="https://eawd.me">ارجع للرئيسية</a>
+        |
+        <a href="https://ar.wikipedia.org/wiki/%D8%B3%D9%86%D8%A9_%D9%83%D8%A8%D9%8A%D8%B3%D8%A9" target="_blank">معلومات اكتر</a>
     </div>
     );
 };
